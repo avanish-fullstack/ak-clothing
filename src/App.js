c58a25efe5b443cc69e4c8a84fe3d200693ed32b
avanish kumar <avanish_tech@outlook.com>
2020-05-10 14:21:29 -0400

    added jeader component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css'
 import Home from './pages/home/home.component'
 import { Route, Switch } from 'react-router-dom'
 import ShopPage from './pages/shop/shop.component'
+import Header from './components/header/header.component'
 
 function App() {
     return (
         <div className="App">
+            <Header />
             <Switch>
                 <Route exact path="/" component={Home}></Route>
                 <Route path="/shop" component={ShopPage}></Route>
