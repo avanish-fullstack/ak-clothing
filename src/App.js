7d843026e28477b5914584c90de344e8e798f0d4
avanish kumar <avanish_tech@outlook.com>
2020-05-11 06:03:53 -0400

    added custom button and atyles

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './pages/home/home.component'
 import { Route, Switch } from 'react-router-dom'
 import ShopPage from './pages/shop/shop.component'
 import Header from './components/header/header.component'
+import SignInAndSignUp from './pages/sign-in-and-signup/sign-in-and-signup'
 
 function App() {
     return (
@@ -12,6 +13,7 @@ function App() {
             <Switch>
                 <Route exact path="/" component={Home}></Route>
                 <Route path="/shop" component={ShopPage}></Route>
+                <Route path="/signin" component={SignInAndSignUp}></Route>
             </Switch>
         </div>
     )
