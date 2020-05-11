0b495242e97c9b82e5706a18d2f81631c317399b
avanish kumar <avanish_tech@outlook.com>
2020-05-11 07:35:53 -0400

    integrated firebase google authentication

--- src/App.js
+++ src/App.js
@@ -5,18 +5,42 @@ import { Route, Switch } from 'react-router-dom'
 import ShopPage from './pages/shop/shop.component'
 import Header from './components/header/header.component'
 import SignInAndSignUp from './pages/sign-in-and-signup/sign-in-and-signup'
+import { auth } from './firebase/firebase.utils'
 
-function App() {
-    return (
-        <div className="App">
-            <Header />
-            <Switch>
-                <Route exact path="/" component={Home}></Route>
-                <Route path="/shop" component={ShopPage}></Route>
-                <Route path="/signin" component={SignInAndSignUp}></Route>
-            </Switch>
-        </div>
-    )
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            currentUser: null,
+        }
+    }
+
+    unsubscribefromauth = null
+
+    componentDidMount() {
+        this.unsubscribefromauth = auth.onAuthStateChanged((user) => {
+            this.setState({ currentUser: user })
+
+            console.log(user)
+        })
+    }
+
+    componentWillUnmount() {
+        this.unsubscribefromauth()
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <Header currentUser={this.state.currentUser} />
+                <Switch>
+                    <Route exact path="/" component={Home}></Route>
+                    <Route path="/shop" component={ShopPage}></Route>
+                    <Route path="/signin" component={SignInAndSignUp}></Route>
+                </Switch>
+            </div>
+        )
+    }
 }
 
 export default App
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import FormInput from '../form-input/form-input.component'
 import './sign-in.styles.scss'
 import CustomButton from '../custom-button/custom-button.component'
+import { signInWithGoogle } from '../../firebase/firebase.utils'
 
 class SignIn extends React.Component {
     constructor() {
@@ -40,7 +41,16 @@ class SignIn extends React.Component {
                         label="password"
                         required
                     />
-                    <CustomButton type="submit">Sign In</CustomButton>
+                    <div className="buttons">
+                        <CustomButton type="submit">Sign In</CustomButton>
+                        <CustomButton
+                            onClick={signInWithGoogle}
+                            type="submit"
+                            isGooglesignIn
+                        >
+                            Sign In with Google
+                        </CustomButton>
+                    </div>
                 </form>
             </div>
         )
